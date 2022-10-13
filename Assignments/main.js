dec264c4049afea2bdd2fa16f9052df751b3b035
alci1167 <97721588+alci1167@users.noreply.github.com>
2022-10-13 17:34:48 -0600

    Assignment 8 update

--- Assignments/main.js
+++ Assignments/main.js
@@ -1,35 +1,42 @@
-const customName = document.getElementById('customname');
-const randomize = document.querySelector('.randomize');
-const story = document.querySelector('.story');
+var customName = document.getElementById('customname');
+var randomize = document.querySelector('.randomize');
+var story = document.querySelector('.story');
 
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
 
-const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
-const newstory = storyText;
-const xItem = randomValueFromArray(insertX);
-const yItem = randomValueFromArray(insertY);
-const zItem = randomValueFromArray(insertZ);
+var storyText = 'It was 0 fahrenheit outside, so :insertx: stayed home and sat by the fireplace. When the wood stopped burning, :inserty: appeared. They stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 90 pounds, and it was a cold day.';
+var insertX = ['Wellington the Skellington', 'Small Big Daddy', 'Chuy the Chihuahua'];
+var insertY = ['Big Foot', 'Mr.Freeze', 'a huge Frosty the Snowman'];
+var insertZ = ['turned to stone', 'was spontaneously covered in frostbite', 'turned into an ice cube'];
+
 randomize.addEventListener('click', result);
 
 function result() {
+  
+  var newstory = storyText;
+  var xItem = randomValueFromArray(insertX);
+  var yItem = randomValueFromArray(insertY);
+  var zItem = randomValueFromArray(insertZ);
+  newstory = newstory.replace(':insertx:', xItem);
+  newstory = newstory.replace(':inserty:', yItem);
+  newstory = newstory.replace(':insertz:', zItem);
+  newstory = newstory.replace(':insertx:', xItem);
 
   if(customName.value !== '') {
     const name = customName.value;
-
+    newstory = newstory.replace('Bob', name);
   }
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
+  if(document.getElementById('uk').checked) {
+    const weight = Math.round(90*0.0714286)+' stone';
+    const temperature =  Math.round((0-32)*5/9)+' centigrade';
+    newstory = newstory.replace('0 fahrenheit', temperature)
+    newstory = newstory.replace('90 pounds', weight)
   }
 
-  story.textContent = ;
+  story.textContent = newstory;
   story.style.visibility = 'visible';
 }
