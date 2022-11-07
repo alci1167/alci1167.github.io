fb876d42e485f53151cc1cd52be7948c8076c448
alci1167 <97721588+alci1167@users.noreply.github.com>
2022-11-06 22:04:39 -0700

    assignment 10

---
+++ Assignments/Assignment10/assignment10.js
@@ -0,0 +1,41 @@
+{
+    "companyName" : "Super Hero Squad",
+    "website" : "Metro City",
+    "employees" : [
+      {
+        "firstName" : "Sam",
+        "department" : "Tech",
+        "designation" : "Manager",
+        "salary" : 40000,
+        "raiseEligible" : true,
+        "wfh" : true
+      },
+
+      {
+        "firstName" : "Mary",
+        "department" : "Finance",
+        "designation" : "Trainee",
+        "salary" : 18500,
+        "raiseEligible" : true,
+        "wfh" : false
+      },
+
+      {
+        "firstName" : "Bill",
+        "department" : "HR",
+        "designation" : "Executive",
+        "salary" : 21200,
+        "raiseEligible" : false,
+        "wfh" : false
+      },
+
+      {
+        "firstName" : "Anna",
+        "department" : "Tech",
+        "designation" : "Executive",
+        "salary" : 25600,
+        "raiseEligible" : false,
+        "wfh" : true
+      }
+    ]
+  }
