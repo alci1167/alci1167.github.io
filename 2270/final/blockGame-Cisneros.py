3723f0f4652b4dec3ccec0d5ad7b5c0cbeaf2838
alci1167 <97721588+alci1167@users.noreply.github.com>
2023-03-30 09:00:38 -0600

    Update blockGame-Cisneros.py

--- 2270/final/blockGame-Cisneros.py
+++ 2270/final/blockGame-Cisneros.py
@@ -1,17 +1,25 @@
-with open("blocks.in", "r") as fin:
-    # Read the number of words.
-    n = int(fin.readline().strip())
+from typing import List   # import List type from the typing module
 
-    # Initialize a dictionary to store the maximum number of times each letter appears in the blocks.
-    blocks = {}
+def count_freq(s: str) -> List[int]:
+	# this function takes a string s and returns a list of 26 integers representing the frequency of each letter (a-z) in s
+	freq = [0] * 26   # initialize a list of 26 integers with value 0
+	for c in s:
+		freq[ord(c) - ord("a")] += 1   # increment the frequency of the letter at index ord(c) - ord("a") in freq
+	return freq   # return the list of frequencies
 
-    # Read each pair of words and update the blocks dictionary.
-    for i in range(n):
-        word1, word2 = fin.readline().strip().split()
-        for letter in "abcdefghijklmnopqrstuvwxyz":
-            blocks[letter] = max(blocks.get(letter, 0), max(word1.count(letter), word2.count(letter)))
+with open("blocks.in") as read:
+	# open the input file "blocks.in" and read the number of words n
+	n = int(read.readline())
+	# read the n words as a list of pairs into the variable words
+	words = [read.readline().split() for _ in range(n)]
 
-with open("blocks.out", "w") as fout:
-    # Write the maximum number of blocks for each letter to the output file.
-    for letter in "abcdefghijklmnopqrstuvwxyz":
-        fout.write(str(blocks[letter]) + "\n")
+max_blocks = [0] * 26   # initialize a list of 26 integers with value 0
+for w1, w2 in words:    # iterate over each pair of words in words
+	freq1, freq2 = count_freq(w1), count_freq(w2)   # calculate the frequency of letters in each word using count_freq
+	for c in range(26):   # iterate over the indices of max_blocks (0-25)
+		max_blocks[c] += max(freq1[c], freq2[c])   # add the maximum frequency of the letter at index c in freq1 and freq2 to max_blocks[c]
+
+with open("blocks.out", "w") as written:
+	# open the output file "blocks.out" for writing
+	for i in max_blocks:
+		print(i, file=written)   # write each element of max_blocks to the output file
