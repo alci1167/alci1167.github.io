ef2a7bace6f4ce3939629f7f23cdfeec03573ebd
alci1167 <97721588+alci1167@users.noreply.github.com>
2023-04-13 22:45:48 -0600

    Update advertisement-Cisneros.py

--- 2270/advertisement-Cisneros.py
+++ 2270/advertisement-Cisneros.py
@@ -1,16 +1,37 @@
-class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
-        N = len(heights)
-        maxa = 0
-        sta = []
-        for i in range(N):
-            start = i
-            while sta and heights[i] < sta[-1][1]:
-                cur = sta.pop()
-                start = cur[0]
-                maxa = max(maxa, (i - cur[0]) * cur[1])
-            sta.append((start, heights[i]))
-        while sta:
-            cur = sta.pop()
-            maxa = max(maxa, (N - cur[0]) * cur[1])
-        return maxa
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+
+#initialize pre and suf lists
+pre = [0] * n  #store the length of the maximum left-subarray ending at i
+suf = [0] * (n + 1)  #store the length of the maximum right-subarray starting at i
+
+ans = 0
+q = []  #stack to store elements and indices
+
+#loop to calc pre array
+for i in range(n):
+    while q and q[-1][0] >= a[i]:  #pop elements from stack until a[i] is greater
+        q.pop()
+    x = -1
+    if q:  #get the index of the top element in the stack
+        x = q[-1][1]
+    pre[i] = 1 + i - x - 1  #calc the length of left-subarray ending at i
+    q.append((a[i], i))  #push current element and its index to stack
+
+q = []  #clear the stack
+
+# Loop to calculate suf array
+for i in range(n - 1, -1, -1):  #iterate backwards from n-1 to 0
+    while q and q[-1][0] >= a[i]:  #pop elements from stack until a[i] is greater
+        q.pop()
+    x = n
+    if q:  #get index of the top element in the stack
+        x = q[-1][1]
+    suf[i] = 1 + x - i - 1  #calculate the length of right-subarray starting at i
+    q.append((a[i], i))  #push current element and its index to stack
+    ans = max(ans, (pre[i] + suf[i] - 1) * a[i])  #calculate the maximum area
+
+print(ans)
